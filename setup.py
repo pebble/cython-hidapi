37b05945d35f9d2e752fb9d707f7de0ba94b940d
Liam McLoughlin <hexxeh@hexxeh.net>
2015-02-09 19:21:14 +0000

    Merge setup scripts

--- setup.py
+++ setup.py
@@ -2,10 +2,29 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import os
-os.environ['CFLAGS'] = "-I/usr/include/libusb-1.0"
-os.environ['LDFLAGS'] = "-L/usr/lib/i386-linux-gnu -lusb-1.0 -ludev -lrt"
+import platform
 
-setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("hid", ["hid.pyx", "hid-libusb.c"])]
-)
+print platform.system()
+
+if platform.system() == 'Darwin':
+	os.environ['CFLAGS'] = "-framework IOKit -framework CoreFoundation"
+	os.environ['LDFLAGS'] = ""
+
+	setup(
+	    cmdclass = {'build_ext': build_ext},
+	    ext_modules = [Extension("hid", ["hid.pyx", "hid-mac.c"])]
+	)
+elif platform.system() == 'Windows':
+	setup(
+	    cmdclass = {'build_ext': build_ext},
+	    ext_modules = [Extension("hid", ["hid.pyx", "hid-windows.c"],
+	                             libraries=["setupapi"])]
+	)
+else:
+	os.environ['CFLAGS'] = "-I/usr/include/libusb-1.0"
+	os.environ['LDFLAGS'] = "-L/usr/lib/i386-linux-gnu -lusb-1.0 -ludev -lrt"
+
+	setup(
+	    cmdclass = {'build_ext': build_ext},
+	    ext_modules = [Extension("hid", ["hid.pyx", "hid-libusb.c"])]
+	)
